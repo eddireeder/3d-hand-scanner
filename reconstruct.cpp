af5ded78b16481fbd69acf32f2d11cac04ab9781
Eddie Reeder <e.reeder1997@gmail.com>
2019-02-22 00:57:58 +0000

    Started reading video file

--- reconstruct.cpp
+++ reconstruct.cpp
@@ -1,3 +1,5 @@
+#include "opencv2/opencv.hpp"
+
 #include <stdio.h>
 #include <iostream>
 #include <array>
@@ -7,7 +9,37 @@
 int main(int argc, const char** argv) {
 
   // Retrieve input video and deconstruct
-  std::cout << argv << '\n';
+  cv::VideoCapture cap(argv[1]);
+  if (!cap.isOpened()) {
+    std::cout << "Failed to open file: " << argv[1] << std::endl;
+    return 0;
+  }
+
+  while(true) {
+    // Capture frame by frame
+    cv::Mat frame;
+    cap >> frame;
+
+    // If frame is empty, break loop
+    if (frame.empty()) {
+      break;
+    }
+
+    // Display the frame
+    imshow("Frame", frame);
+
+    // Exit when ESC pressed
+    char c = (char) cv::waitKey(25);
+    if (c == 27) {
+      break;
+    }
+  }
+
+  // Release video capture
+  cap.release();
+
+  // Close all frames
+  cv::destroyAllWindows();
 
   return 0;
 }
